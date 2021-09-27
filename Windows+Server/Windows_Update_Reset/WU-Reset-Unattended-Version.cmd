815fbc660fd5438af48f1224d129a699972b3417
Alex Ivantsov <alex@ivantsov.tech>
2021-09-27 10:53:12 -0400

    Fixed Automated Script

--- Windows+Server/Windows_Update_Reset/WU-Reset-Unattended-Version.cmd
+++ Windows+Server/Windows_Update_Reset/WU-Reset-Unattended-Version.cmd
@@ -1,122 +1,235 @@
 :: ==================================================================================
 :: NAME:	Reset Windows Update Tool - Bastardized by Alex
-:: DESCRIPTION:	This script reset the Windows Update Components.
-:: AUTHOR(s):	Manuel Gil. Alex Ivantsov
+:: DESCRIPTION:	This script reset the Windows Update Components automagically without any user input required.
+:: AUTHOR(s):	Manuel Gil. + (Edited by) Alex Ivantsov
 :: VERSION:	10.5.4.1 - Date: 9/13/2021
 :: Main modification includes ability to run main portion of the script as unatended with Datto RMM or other deployment solution.
-:: - Asumes you are already running script as Admin.
+:: - Asumes you are already running script as Admin. Will Terminate itself if Admin Permission is not detected.
 :: ==================================================================================
 
-:: Load the system values.
-:: void getValues();
-:: /************************************************************************************/
-:getValues
-	for /f "tokens=4 delims=[] " %%a in ('ver') do set version=%%a
+@echo off
 
-	if %version% EQU 6.0.6000 (
-		:: Name: "Microsoft Windows Vista"
-		set name=Microsoft Windows Vista
-		:: Family: Windows 6
-		set family=6
-		:: Compatibility: No
-		set allow=No
-	) else if %version% EQU 6.0.6001 (
-		:: Name: "Microsoft Windows Vista SP1"
-		set name=Microsoft Windows Vista SP1
-		:: Family: Windows 6
-		set family=6
-		:: Compatibility: No
-		set allow=No
-	) else if %version% EQU 6.0.6002 (
-		:: Name: "Microsoft Windows Vista SP2"
-		set name=Microsoft Windows Vista SP2
-		:: Family: Windows 6
-		set family=6
-		:: Compatibility: No
-		set allow=No
-	) else if %version% EQU 6.1.7600 (
-		:: Name: "Microsoft Windows 7"
-		set name=Microsoft Windows 7
-		:: Family: Windows 7
-		set family=7
-		:: Compatibility: No
-		set allow=No
-	) else if %version% EQU 6.1.7601 (
-		:: Name: "Microsoft Windows 7 SP1"
-		set name=Microsoft Windows 7 SP1
-		:: Family: Windows 7
-		set family=7
-		:: Compatibility: No
-		set allow=No
-	) else if %version% EQU 6.2.9200 (
-		:: Name: "Microsoft Windows 8"
-		set name=Microsoft Windows 8
-		:: Family: Windows 8
-		set family=8
-		:: Compatibility: Yes
-		set allow=Yes
-	) else if %version% EQU 6.3.9200 (
-		:: Name: "Microsoft Windows 8.1"
-		set name=Microsoft Windows 8.1
-		:: Family: Windows 8
-		set family=8
-		:: Compatibility: Yes
-		set allow=Yes
-	) else if %version% EQU 6.3.9600 (
-		:: Name: "Microsoft Windows 8.1 Update 1"
-		set name=Microsoft Windows 8.1 Update 1
-		:: Family: Windows 8
-		set family=8
-		:: Compatibility: Yes
-		set allow=Yes
-	) else (
-		ver | find "10.0." > nul
-		if %errorlevel% EQU 0 (
-			:: Name: "Microsoft Windows 10"
-			set name=Microsoft Windows 10
-			:: Family: Windows 10
-			set family=10
-			:: Compatibility: Yes
-			set allow=Yes
-		) else (
-			:: Name: "Unknown"
-			set name=Unknown
-			:: Compatibility: No
-			set allow=No
+:: Checking for Administrator elevation.
+:: void permission();
+:: /************************************************************************************/
+
+	openfiles>nul 2>&1
+
+	if %errorlevel% EQU 0 goto WUReset
+
+	call :print Checking for Administrator elevation.
+
+	echo.
+	echo.    You are not running as Administrator.
+	echo.    This tool cannot do it's job without elevation.
+	echo.
+	echo.    You need run this tool as Administrator.
+	echo.
+
+exit
+:: /************************************************************************************/
+
+
+:: Run the reset Windows Update components.
+:: void components();
+:: /*************************************************************************************/
+	
+	:: ----- Stopping the Windows Update services -----
+
+:WUReset
+	echo Stopping the Windows Update services.
+	net stop bits /y
+
+	echo Stopping the Windows Update services.
+	net stop wuauserv /y
+
+	echo Stopping the Windows Update services.
+	net stop appidsvc /y
+
+	echo Stopping the Windows Update services.
+	net stop cryptsvc /y
+
+	echo Canceling the Windows Update process.
+	taskkill /im wuauclt.exe /f
+	taskkill /im wuauserv.exe /f
+
+
+	:: ----- Checking the services status -----
+	echo Checking the services status.
+
+	sc query bits | findstr /I /C:"STOPPED"
+	if %errorlevel% NEQ 0 (
+		echo.    Failed to stop the BITS service.
+		echo.
+		echo.Press any key to continue . . .
+		pause>nul
+		goto :eof
+	)
+
+	echo Checking the services status.
+
+	sc query wuauserv | findstr /I /C:"STOPPED"
+	if %errorlevel% NEQ 0 (
+		echo.    Failed to stop the Windows Update service.
+		echo.
+		echo.Press any key to continue . . .
+		pause>nul
+		goto :eof
+	)
+
+	echo Checking the services status.
+
+	sc query appidsvc | findstr /I /C:"STOPPED"
+	if %errorlevel% NEQ 0 (
+		sc query appidsvc | findstr /I /C:"OpenService FAILED 1060"
+		if %errorlevel% NEQ 0 (
+			echo.    Failed to stop the Application Identity service.
 		)
 	)
 
-	call :print %name% detected . . .
+	echo Checking the services status.
 
-	if %allow% EQU Yes goto permission
+	sc query cryptsvc | findstr /I /C:"STOPPED"
+	if %errorlevel% NEQ 0 (
+		echo.    Failed to stop the Cryptographic Services service.
+	)
 
-	call :print Sorry, this Operative System is not compatible with this tool.
+	:: ----- Delete the qmgr*.dat files -----
+	echo Deleting the qmgr*.dat files.
 
-	echo.    An error occurred while attempting to verify your system.
-	echo.    Can this using a business or test version.
-	echo.
-	echo.    if not, verify that your system has the correct security fix.
-
-	echo.
-
-::	echo.Press any key to continue . . .
-::	pause>nul
-:: goto :eof
-:: /************************************************************************************/
+	del /s /q /f "%ALLUSERSPROFILE%\Application Data\Microsoft\Network\Downloader\qmgr*.dat"
+	del /s /q /f "%ALLUSERSPROFILE%\Microsoft\Network\Downloader\qmgr*.dat"
 
 
-:: Print Top Text.
-::		@param - text = the text to print (%*).
-:: void print(string text);
+	:: ----- Renaming the softare distribution folders backup copies -----
+	echo Deleting the old software distribution backup copies.
+
+	cd /d %SYSTEMROOT%
+
+	if exist "%SYSTEMROOT%\winsxs\pending.xml.bak" (
+		del /s /q /f "%SYSTEMROOT%\winsxs\pending.xml.bak"
+	)
+	if exist "%SYSTEMROOT%\SoftwareDistribution.bak" (
+		rmdir /s /q "%SYSTEMROOT%\SoftwareDistribution.bak"
+	)
+	if exist "%SYSTEMROOT%\system32\Catroot2.bak" (
+		rmdir /s /q "%SYSTEMROOT%\system32\Catroot2.bak"
+	)
+	if exist "%SYSTEMROOT%\WindowsUpdate.log.bak" (
+		del /s /q /f "%SYSTEMROOT%\WindowsUpdate.log.bak"
+	)
+
+	echo Renaming the software distribution folders.
+
+	if exist "%SYSTEMROOT%\winsxs\pending.xml" (
+		takeown /f "%SYSTEMROOT%\winsxs\pending.xml"
+		attrib -r -s -h /s /d "%SYSTEMROOT%\winsxs\pending.xml"
+		ren "%SYSTEMROOT%\winsxs\pending.xml" pending.xml.bak
+	)
+	if exist "%SYSTEMROOT%\SoftwareDistribution" (
+		attrib -r -s -h /s /d "%SYSTEMROOT%\SoftwareDistribution"
+		ren "%SYSTEMROOT%\SoftwareDistribution" SoftwareDistribution.bak
+		if exist "%SYSTEMROOT%\SoftwareDistribution" (
+			echo.
+			echo.    Failed to rename the SoftwareDistribution folder.
+			echo.
+		)
+	)
+	if exist "%SYSTEMROOT%\system32\Catroot2" (
+		attrib -r -s -h /s /d "%SYSTEMROOT%\system32\Catroot2"
+		ren "%SYSTEMROOT%\system32\Catroot2" Catroot2.bak
+	)
+	if exist "%SYSTEMROOT%\WindowsUpdate.log" (
+		attrib -r -s -h /s /d "%SYSTEMROOT%\WindowsUpdate.log"
+		ren "%SYSTEMROOT%\WindowsUpdate.log" WindowsUpdate.log.bak
+	)
+
+	:: ----- Reset the BITS service and the Windows Update service to the default security descriptor -----
+	echo Reset the BITS service and the Windows Update service to the default security descriptor.
+
+	sc.exe sdset wuauserv D:(A;CI;CCLCSWRPLORC;;;AU)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;SY)S:(AU;FA;CCDCLCSWRPWPDTLOSDRCWDWO;;;WD)
+	sc.exe sdset bits D:(A;CI;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)S:(AU;SAFA;WDWO;;;BA)
+	sc.exe sdset cryptsvc D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)(A;;CCLCSWRPWPDTLOCRRC;;;SO)(A;;CCLCSWLORC;;;AC)(A;;CCLCSWLORC;;;S-1-15-3-1024-3203351429-2120443784-2872670797-1918958302-2829055647-4275794519-765664414-2751773334)
+	sc.exe sdset trustedinstaller D:(A;CI;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;SY)(A;;CCDCLCSWRPWPDTLOCRRC;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)S:(AU;SAFA;WDWO;;;BA)
+
+	:: ----- Reregister the BITS files and the Windows Update files -----
+	echo Reregister the BITS files and the Windows Update files.
+
+	cd /d %SYSTEMROOT%\system32
+	regsvr32.exe /s atl.dll
+	regsvr32.exe /s urlmon.dll
+	regsvr32.exe /s mshtml.dll
+	regsvr32.exe /s shdocvw.dll
+	regsvr32.exe /s browseui.dll
+	regsvr32.exe /s jscript.dll
+	regsvr32.exe /s vbscript.dll
+	regsvr32.exe /s scrrun.dll
+	regsvr32.exe /s msxml.dll
+	regsvr32.exe /s msxml3.dll
+	regsvr32.exe /s msxml6.dll
+	regsvr32.exe /s actxprxy.dll
+	regsvr32.exe /s softpub.dll
+	regsvr32.exe /s wintrust.dll
+	regsvr32.exe /s dssenh.dll
+	regsvr32.exe /s rsaenh.dll
+	regsvr32.exe /s gpkcsp.dll
+	regsvr32.exe /s sccbase.dll
+	regsvr32.exe /s slbcsp.dll
+	regsvr32.exe /s cryptdlg.dll
+	regsvr32.exe /s oleaut32.dll
+	regsvr32.exe /s ole32.dll
+	regsvr32.exe /s shell32.dll
+	regsvr32.exe /s initpki.dll
+	regsvr32.exe /s wuapi.dll
+	regsvr32.exe /s wuaueng.dll
+	regsvr32.exe /s wuaueng1.dll
+	regsvr32.exe /s wucltui.dll
+	regsvr32.exe /s wups.dll
+	regsvr32.exe /s wups2.dll
+	regsvr32.exe /s wuweb.dll
+	regsvr32.exe /s qmgr.dll
+	regsvr32.exe /s qmgrprxy.dll
+	regsvr32.exe /s wucltux.dll
+	regsvr32.exe /s muweb.dll
+	regsvr32.exe /s wuwebv.dll
+
+	:: ----- Resetting Winsock -----
+	echo Resetting Winsock.
+	netsh winsock reset
+
+	:: ----- Resetting WinHTTP Proxy -----
+	echo Resetting WinHTTP Proxy.
+
+	netsh winhttp reset proxy
+
+
+	:: ----- Set the startup type as automatic -----
+	echo Resetting the services as automatics.
+	sc.exe config wuauserv start= auto
+	sc.exe config bits start= delayed-auto
+	sc.exe config cryptsvc start= auto
+	sc.exe config TrustedInstaller start= demand
+	sc.exe config DcomLaunch start= auto
+
+	:: ----- Starting the Windows Update services -----
+	echo Starting the Windows Update services.
+	net start bits
+
+	echo Starting the Windows Update services.
+	net start wuauserv
+
+	echo Starting the Windows Update services.
+	net start appidsvc
+
+	echo Starting the Windows Update services.
+	net start cryptsvc
+
+	echo Starting the Windows Update services.
+	net start DcomLaunch
+
+	:: ----- End process -----
+	echo The operation completed successfully.
+
 :: /*************************************************************************************/
-:print
-	cls
-	echo.
-	echo.%name% [Version: %version%]
-	echo.Reset Windows Update Tool.
-	echo.
-	echo.%*
-	echo.
-:: goto :eof
-:: /*************************************************************************************/
 
+:eof
