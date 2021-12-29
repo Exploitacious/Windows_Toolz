f31d25b45674568ccc224af49c12a9e62159ec70
Alex Ivantsov <alex@ivantsov.tech>
2021-12-28 19:06:51 -0500

    Added a one-click script runner

---
+++ Windows_Update_Reset/Run-WUReset.cmd
@@ -0,0 +1,34 @@
+:: Download and run the system debloat scripts directly from GitHub
+:: Created by: Alex Ivantsov
+
+@echo off
+
+:: Checking for Administrator elevation.
+
+        openfiles>nul 2>&1
+
+        if %errorlevel% EQU 0 goto :Download
+
+        echo.
+        echo.
+        echo.    You are not running as Administrator.
+        echo.    This script cannot do it's job without elevation.
+        echo.
+        echo.    You need run this tool as Administrator.
+        echo.
+
+    exit
+
+
+:: Download Required Files from https://github.com/Exploitacious/Windows_Toolz/tree/main/Windows%2BServer/System_Debloat
+:Download
+
+    PowerShell -executionpolicy bypass -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Exploitacious/Windows_Toolz/main/Windows_Update_Reset/WU-Reset-Unattended-Version.cmd', 'WU-Reset-Unattended-Version.cmd')"
+
+
+:: Start Running the SYSTEM DEBLOAT scripts
+:RunScript
+
+    SET ThisScriptsDirectory=%~dp0
+
+    WU-Reset-Unattended-Version.cmd
