d2693ce7ca729611060b71f84476a5668cfd2cba
Alex Ivantsov <Alex@ivantsov.tech>
2024-07-09 15:41:14 -0400

    Updated - added more functionality

--- Production/General/PSModuleTerminator/PSModuleTerminator.ps1
+++ Production/General/PSModuleTerminator/PSModuleTerminator.ps1
@@ -1,5 +1,5 @@
 Write-Host
-Write-Host " -= PowerShell Module Terminator =- " -ForegroundColor DarkGreen
+Write-Host " -= PowerShell Module Terminator =- " -ForegroundColor DarkMagenta
 Write-Host
 
 
@@ -7,6 +7,98 @@ Write-Host
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit 
+}
+
+# Function to restore PowerShell to stock by removing all non-default modules
+function Restore-DefaultModules {
+    Write-Host "Restoring PowerShell to stock settings by removing all non-default modules..."
+    
+    $defaultModules = @(
+        "AppBackgroundTask",
+        "Appx",
+        "AssignedAccess",
+        "BitLocker",
+        "BitsTransfer",
+        "BranchCache",
+        "CimCmdlets",
+        "ClusterAwareUpdating",
+        "Defender",
+        "DeliveryOptimization",
+        "DirectAccessClientComponents",
+        "Dism",
+        "DnsClient",
+        "EventTracingManagement",
+        "HgsClient",
+        "HostComputeService",
+        "Hyper-V",
+        "International",
+        "ISE",
+        "Kds",
+        "Microsoft.PowerShell.Diagnostics",
+        "Microsoft.PowerShell.Host",
+        "Microsoft.PowerShell.Management",
+        "Microsoft.PowerShell.Security",
+        "Microsoft.PowerShell.Utility",
+        "Microsoft.WSMan.Management",
+        "MMAgent",
+        "MsDtc",
+        "NetAdapter",
+        "NetConnection",
+        "NetEventPacketCapture",
+        "NetLbfo",
+        "NetNat",
+        "NetQos",
+        "NetSecurity",
+        "NetSwitchTeam",
+        "NetTCPIP",
+        "NetWNV",
+        "NetworkConnectivityStatus",
+        "NetworkSwitchManager",
+        "NetworkSwitchSubsystem",
+        "PcsvDevice",
+        "Pester",
+        "PKI",
+        "PnpDevice",
+        "PrintManagement",
+        "ProcessMitigations",
+        "Provisioning",
+        "PSDesiredStateConfiguration",
+        "PSDiagnostics",
+        "PSScheduledJob",
+        "PSWorkflow",
+        "PSWorkflowUtility",
+        "ScheduledTasks",
+        "SecureBoot",
+        "SmbShare",
+        "SmbWitness",
+        "StartLayout",
+        "Storage",
+        "TLS",
+        "TroubleshootingPack",
+        "TrustedPlatformModule",
+        "VpnClient",
+        "Wdac",
+        "WindowsDeveloperLicense",
+        "WindowsErrorReporting",
+        "WindowsSearch",
+        "WindowsUpdate",
+        "WindowsUpdateProvider"
+    )
+
+    $installedModules = Get-Module -ListAvailable | Select-Object -ExpandProperty Name | Sort-Object -Unique
+
+    $modulesToRemove = $installedModules | Where-Object { $defaultModules -notcontains $_ }
+
+    foreach ($module in $modulesToRemove) {
+        Write-Host "Removing module: $module"
+        Stop-ModuleProcesses -moduleName $module
+        Remove-Module -Name $module -Force -ErrorAction SilentlyContinue
+        $modulePath = "C:\Program Files\WindowsPowerShell\Modules\$module"
+        EnsurePermissions -modulePath $modulePath
+        Remove-ModuleFiles -moduleName $module
+    }
+
+    Write-Host "PowerShell has been restored to stock settings."
 }
 
 # Function to check for running processes using the module
@@ -124,24 +216,56 @@ function Restart-TerminalProcesses {
 # Main script execution
 #######################
 
-$moduleName = Read-Host "Enter the name of the module"
-$moduleVersion = Read-Host "Enter the version of the module (leave blank for all versions)"
+while ($true) {
+    $input = Read-Host "Enter the name of the module to begin removal ; 'RESTORE' to reset PowerShell to stock settings ; or 'q' to quit and reset terminal"
 
-if ($moduleVersion) {
-    $fullModuleName = "$moduleName -RequiredVersion $moduleVersion"
-}
-else {
-    $fullModuleName = $moduleName
+    if ($input -eq "q") {
+        Write-Host "Exiting script."
+        break
+    }
+
+    if ($input -eq "RESTORE") {
+        Restore-DefaultModules
+        Write-Host "Restoration complete."
+    }
+    else {
+        $moduleName = $input
+
+        $availableVersions = Get-Module -ListAvailable -Name $moduleName | Select-Object -ExpandProperty Version
+
+        if ($availableVersions.Count -eq 0) {
+            Write-Host "No versions of the module '$moduleName' found."
+        }
+        else {
+            Write-Host "Available versions of the module '$moduleName':"
+            $availableVersions | ForEach-Object { Write-Host $_ }
+
+            $versionInput = Read-Host "Enter the version to uninstall (or type 'ALL' to uninstall all versions)"
+
+            if ($versionInput -eq "ALL") {
+                foreach ($version in $availableVersions) {
+                    Write-Host "Starting module removal process for $moduleName version $version..."
+                    Stop-ModuleProcesses -moduleName $moduleName
+                    Remove-Module -Name $moduleName -RequiredVersion $version -Force -ErrorAction SilentlyContinue
+                    $modulePath = "C:\Program Files\WindowsPowerShell\Modules\$moduleName\$version"
+                    EnsurePermissions -modulePath $modulePath
+                    Remove-ModuleFiles -moduleName $moduleName
+                    Write-Host "Module removal process completed for $moduleName version $version."
+                }
+            }
+            else {
+                Write-Host "Starting module removal process for $moduleName version $versionInput..."
+                Stop-ModuleProcesses -moduleName $moduleName
+                Remove-Module -Name $moduleName -RequiredVersion $versionInput -Force -ErrorAction SilentlyContinue
+                $modulePath = "C:\Program Files\WindowsPowerShell\Modules\$moduleName\$versionInput"
+                EnsurePermissions -modulePath $modulePath
+                Remove-ModuleFiles -moduleName $moduleName
+                Write-Host "Module removal process completed for $moduleName version $versionInput." -ForegroundColor DarkGreen
+            }
+        }
+    }
 }
 
-Write-Host "Starting module removal process for $fullModuleName..." -ForegroundColor Green
-Stop-ModuleProcesses -moduleName $moduleName
-Remove-Module -Name $moduleName -Verbose -Force -ErrorAction SilentlyContinue
-$modulePath = "C:\Program Files\WindowsPowerShell\Modules\$moduleName"
-EnsurePermissions -modulePath $modulePath
-Remove-ModuleFiles -moduleName $moduleName
-Write-Host
-Write-Host "Module removal process completed for $fullModuleName." -ForegroundColor DarkGreen
 Write-Host
 Write-Host "Restarting Terminal in 5 seconds..."
 
