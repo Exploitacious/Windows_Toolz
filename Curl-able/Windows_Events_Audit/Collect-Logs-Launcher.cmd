739ed08a3e0eaaa061e46da541be7acb5cb1771b
alex@ivantsov.tech <alex@ivantsov.tech>
2025-05-19 13:17:30 -0400

    Updated Launcher URLs

--- Curl-able/Windows_Events_Audit/Collect-Logs-Launcher.cmd
+++ Curl-able/Windows_Events_Audit/Collect-Logs-Launcher.cmd
@@ -31,9 +31,9 @@
     
     cd "C:\Temp\GatherLogs"
 
-    PowerShell -executionpolicy bypass -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Exploitacious/WindowsEventsToCSVTimeline/master/Gather-LogsToTimeLine.ps1', 'Gather-LogsToTimeLine.ps1')"
+    PowerShell -executionpolicy bypass -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Exploitacious/Windows_Toolz/refs/heads/main/Curl-able/Windows_Events_Audit/Gather-LogsToTimeLine.ps1', 'Gather-LogsToTimeLine.ps1')"
 
-    PowerShell -executionpolicy bypass -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Exploitacious/WindowsEventsToCSVTimeline/master/Parse-LogsToTimeLine.ps1', 'Parse-LogsToTimeLine.ps1')"
+    PowerShell -executionpolicy bypass -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Exploitacious/Windows_Toolz/refs/heads/main/Curl-able/Windows_Events_Audit/Parse-LogsToTimeLine.ps1', 'Parse-LogsToTimeLine.ps1')"
 
 
 :: Start Running the Gather Logs scripts
